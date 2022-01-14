e2d24a23511ff16cdc291430a88d4ed34f721386
pawelek2000 <65534106+pawelek2000@users.noreply.github.com>
2022-01-14 14:45:11 +0100

    Moje przedmioty nauczyciel

---
+++ AJDENTITY/AJDENTITY/Controllers/MojePrzedmiotyNauczycielController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AJDENTITY.Models;
+
+namespace AJDENTITY.Controllers
+{
+    public class MojePrzedmiotyNauczycielController : Controller
+    {
+        private Entities db = new Entities();
+        IdentityManager IM = new IdentityManager();
+        // GET: MojePrzedmiotyNauczyciel
+        public ActionResult Index()
+        {
+            var CurrentUser = IM.GetUserByName(User.Identity.Name);
+            var teacher = db.Teachers.Where(p => p.Account_Id == CurrentUser.Id).ToList()[0];
+            var CTS = db.ClassSubjectTeachers.Where(p => p.Teacher_Id == teacher.Id).ToList();
+            var Przedmioty = new List<Subject>();
+            foreach (var cts in CTS)
+            {
+                Przedmioty.Add(db.Subjects.Where(p => p.Id == cts.Subject_Id).ToList()[0]);
+            }
+            return View(Przedmioty);
+        }
+
+        // GET: MojePrzedmiotyNauczyciel/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Subject subject = db.Subjects.Find(id);
+            if (subject == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subject);
+        }
+
+        // GET: MojePrzedmiotyNauczyciel/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MojePrzedmiotyNauczyciel/Create
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,LearningContent")] Subject subject)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Subjects.Add(subject);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(subject);
+        }
+
+        // GET: MojePrzedmiotyNauczyciel/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Subject subject = db.Subjects.Find(id);
+            if (subject == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subject);
+        }
+
+        // POST: MojePrzedmiotyNauczyciel/Edit/5
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,LearningContent")] Subject subject)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(subject).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(subject);
+        }
+
+        // GET: MojePrzedmiotyNauczyciel/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Subject subject = db.Subjects.Find(id);
+            if (subject == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subject);
+        }
+
+        // POST: MojePrzedmiotyNauczyciel/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Subject subject = db.Subjects.Find(id);
+            db.Subjects.Remove(subject);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
